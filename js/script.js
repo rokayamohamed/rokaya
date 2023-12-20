dd13da6f3f0edbf5bc3b092112ad7903d090db94
Rokaya Mohamed <145031120+rokayamohamed@users.noreply.github.com>
2023-12-20 23:25:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const btn = document.querySelector("header .container .btn");
 const navigatinBar = document.querySelector("header .container .navigation ul");
-
+let navigatin = document.getElementById("header");
 const allLis = document.querySelectorAll("header .container .navigation ul li");
+const scrollProgress = document.querySelector(".scroll-height");
+const totalHeight = document.body.scrollHeight - window.innerHeight;
+const scrollProgressDiv = document.querySelector(".scroll-height > div");
 
 btn.addEventListener("click", function () {
   navigatinBar.classList.toggle("show");
@@ -16,11 +19,23 @@ allLis.forEach(function (e) {
 let scrollbtn = document.querySelector(".scroll");
 
 window.onscroll = function () {
+  if (window.scrollY >= 150) {
+    navigatin.classList.add("fixed");
+  } else {
+    navigatin.classList.remove("fixed");
+  }
   if (window.scrollY >= 600) {
-    scrollbtn.style.display = "block";
+    scrollbtn.style.display = "flex";
   } else {
     scrollbtn.style.display = "none";
   }
+  const progressHeight = (window.pageYOffset / totalHeight) * 100;
+  if (window.scrollY <= 0) {
+    scrollProgress.style.display = "none";
+  } else {
+    scrollProgress.style.display = "block";
+  }
+  scrollProgressDiv.style.width = `${progressHeight}%`;
 };
 
 scrollbtn.onclick = function () {
@@ -34,3 +49,21 @@ scrollbtn.onclick = function () {
 let copyRight = document.querySelector("footer .container .footer-title sub");
 
 copyRight.innerHTML = new Date().getFullYear();
+
+
+
+scrollProgress.style.top = `${
+  document.querySelector("header#header").offsetHeight
+}px`;
+window.onresize = function () {
+  scrollProgress.style.top = `${
+    document.querySelector("header#header").offsetHeight
+  }px`;
+};
+scrollButton.addEventListener("click", function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth"
+  });
+});
